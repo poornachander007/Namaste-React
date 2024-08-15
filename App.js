b433ffe4a78750b4c6f3a035a7d9669a2a77c61c
poornachander007 <poornachander0333@gmail.com>
2024-08-15 23:14:38 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,16 @@
+const heading1 = React.createElement("h1", {}, "Heading - 1");
+const heading2 = React.createElement("h2", {}, "Heading - 2");
+
+const child1 = React.createElement("div", { id: "child-1" }, [
+  heading1,
+  heading2,
+]);
+const child2 = React.createElement("div", { id: "child-2" }, [
+  heading1,
+  heading2,
+]);
+
+const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(parent);
